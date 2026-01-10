f81cbe1134eda782435a6a71bd8a5de74e6abc11
Arpayitri Bhattacharyya <arpayitribhattacharyya090305@gmail.com>
2026-01-10 17:48:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,11 +25,11 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // DB CONNECTION 
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
-  database: process.env.DB_NAME || "college_admission",
-  port: process.env.DB_PORT || 3308,
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASS ,
+  database: process.env.DB_NAME ,
+  port: process.env.DB_PORT ,
   waitForConnections: true,
   connectionLimit: 10,
 });
